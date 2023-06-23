d5928030c99489c94a9e573b170104d70fee7827
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2023-06-23 22:34:05 +0400

    update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -35,7 +35,7 @@ export default function Home() {
                     </div>
                     <div className = {'flex justify-center sm:text-[18px] text-[12px] sm:pb-10 py-2 text-center'}>
                     
-                        <Link to={'/tasks'} className = {'w-1/2 cursor-default sm:px-[100px] py-2 bg-gradient-to-r from-[#ffffff18] to-[#ffffff08] rounded-xl mt-5 backdrop-blur-sm' } >
+                        <Link to={'/tasks'} className = {'w-1/2 cursor-pointer sm:px-[100px] py-2 bg-gradient-to-r from-[#ffffff18] to-[#ffffff08] rounded-xl mt-5 backdrop-blur-sm' } >
                         {"ENTER->"}</Link>
                         
                         
