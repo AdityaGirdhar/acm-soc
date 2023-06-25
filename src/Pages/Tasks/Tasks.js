1e62610361cdf117ea5e1f5c90a9e50c2ae2deba
Divyansh <divyansh.72003@gmail.com>
2023-06-25 23:31:37 +0530

    Added Day-3

--- src/Pages/Tasks/Tasks.js
+++ src/Pages/Tasks/Tasks.js
@@ -36,12 +36,10 @@ export default function Tasks() {
                             Day 2-Programming
                         </a>
                     </div>
-                    <div className = {'flex justify-center sm:text-[18px] text-[12px] sm:pb-2 py-2'}>
-                        <button className = {'w-1/2 cursor-default sm:px-[100px] py-2 bg-gradient-to-r from-[#ffffff18] to-[#ffffff08] rounded-xl mt-5 backdrop-blur-sm' } >
-                            <div className="flex justify-center">
-                                <img src={lock} className={'h-[15px] w-[15px]'}></img>
-                            </div>
-                        </button>
+                    <div className = {'flex justify-center sm:text-[18px] text-[12px] sm:pb-2 py-2 text-center'}>
+                        <a href="https://codeforces.com/contestInvitation/0f49c53d5a81a697bed4f382821415716f2773a4" target="_blank" className = {'w-1/2 cursor-pointer sm:px-[100px] py-2 bg-gradient-to-r from-[#ffffff18] to-[#ffffff08] rounded-xl mt-5 backdrop-blur-sm' } >
+                            Day 3-Programming
+                        </a>
                     </div>
                     <div className = {'flex justify-center sm:text-[18px] text-[12px] sm:pb-2 py-2'}>
                         <button className = {'w-1/2 cursor-default sm:px-[100px] py-2 bg-gradient-to-r from-[#ffffff18] to-[#ffffff08] rounded-xl mt-5 backdrop-blur-sm' } >
